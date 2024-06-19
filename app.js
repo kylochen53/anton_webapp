1429a2ced20a3a926be5e517af76ef1d1c22e938
kylochen53 <134651214+kylochen53@users.noreply.github.com>
2024-06-18 18:14:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-var multipleCardCarousel = document.querySelector(
-  "#carousel_yt"
-);
+document.addEventListener('DOMContentLoaded', function () {
+  var multipleCardCarousel = document.querySelector("#carousel_yt");
 
-if (window.matchMedia("(min-width: 768px)").matches) {
-  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-    interval: false,
-    wrap: false,
-  });
-  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-  var cardWidth = $(".carousel-item").width();
-  var scrollPosition = 0;
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+      wrap: false,
+    });
 
-  $("#carousel_yt.carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * 3) {
-      scrollPosition += cardWidth*3;
-      $("#carousel_yt.carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-  $("#carousel_yt.carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth*3;
-      $("#carousel_yt.carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-} else {
-  $(multipleCardCarousel).addClass("slide");
-}
+    var carouselInner = $(".carousel-inner")[0];
+    var carouselWidth = carouselInner.scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+
+    $(".carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 3) {
+        scrollPosition += cardWidth * 3;
+      } else {
+        scrollPosition = 0;
+      }
+      $(carouselInner).animate({ scrollLeft: scrollPosition }, 600);
+    });
+
+    $(".carousel-control-prev").on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth * 3;
+      } else {
+        scrollPosition = carouselWidth - cardWidth * 3;
+      }
+      $(carouselInner).animate({ scrollLeft: scrollPosition }, 600);
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+});
